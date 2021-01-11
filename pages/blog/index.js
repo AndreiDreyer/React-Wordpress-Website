b30f58643ac039f2cfb748492f150f6a6542c89a
Andrei Dreyer <andrei.dreyer1997@gmail.com>
2021-01-11 20:04:02 +0200

    Basic website pulling blog posts from wordpress is working

---
+++ pages/blog/index.js
@@ -0,0 +1,52 @@
+import Head from 'next/head';
+import Link from 'next/link';
+
+import { getAllPosts } from '../../lib/api';
+import styles from '../../styles/Home.module.css';
+import blogStyles from '../../styles/Blog.module.css';
+
+const Blog = ({ allPosts: { edges } }) => (
+  <div className={styles.container}>
+    <Head>
+      <title>The Salty Zebra</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <main className={styles.main}>
+      <h1 className={styles.title}>Latest Blog Articles</h1>
+      <hr />
+      <section>
+        {edges.map(({ node }) => (
+          <div className={blogStyles.listitem} key={node.id}>
+            <div className={blogStyles.listitem__thumbnail}>
+              <figure>
+                <img
+                  src={node.extraPostInfo.thumbImage.mediaItemUrl}
+                  alt={node.title}
+                />
+              </figure>
+            </div>
+            <div className={blogStyles.listitem__content}>
+              <h2>{node.title}</h2>
+              <p>{node.extraPostInfo.authorExcerpt}</p>
+              <Link href={`/blog/${node.slug}`}>
+                <a>Read More</a>
+              </Link>
+            </div>
+          </div>
+        ))}
+      </section>
+    </main>
+  </div>
+);
+
+export default Blog;
+
+export async function getStaticProps() {
+  const allPosts = await getAllPosts();
+  return {
+    props: {
+      allPosts,
+    },
+  };
+}
