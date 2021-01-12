3010d45d00a68ed74b03740f4016a0cf3815cc52
Andrei Dreyer <andrei.dreyer1997@gmail.com>
2021-01-12 17:16:04 +0200

    Proof of concept for center menu working

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,9 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+
 import Head from 'next/head';
 import Link from 'next/link';
 import styles from '../styles/Home.module.css';
@@ -6,35 +12,54 @@ import Navigation from '../components/Navigation';
 import { Grid } from '@material-ui/core';
 import { getMenu } from '../lib/api';
 
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  appBar: {
+    background: 'transparent',
+    boxShadow: 'none',
+  },
+  phoneButton: {
+    marginRight: 10,
+  },
+  menuGrid: {
+    height: '100vh',
+    display: 'flex',
+  },
+});
+
 export default function Home({ menuItems }) {
+  const classes = useStyles();
+
   return (
-    <Grid container direction="row">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <Navigation menuItems={menuItems}></Navigation>
-      <main className={styles.main}>
-        <h1 className={styles.title}>Welcome to our demo blog</h1>
-        <p>
-          You can find more articles on the{' '}
-          <Link href="/blog">
-            <a>blog articles page</a>
-          </Link>
-        </p>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+    <Grid
+      container
+      sapcing={0}
+      alignItems="center"
+      justify="center"
+      direction="row"
+      className={classes.menuGrid}
+    >
+      {/* Rotating Images */}
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <Grid justify="space-between" container spacing={24}>
+            <Grid item></Grid>
+            <Grid item>
+              <Grid item>
+                <Button variant="outlined" className={classes.phoneButton}>
+                  Phone
+                </Button>
+                <Button variant="outlined">Email</Button>
+              </Grid>
+            </Grid>
+          </Grid>
+          {/* Logo */}
+        </Toolbar>
+      </AppBar>
+      <Grid justify="center">
+        <p>this is the menu</p>
+      </Grid>
     </Grid>
   );
 }
