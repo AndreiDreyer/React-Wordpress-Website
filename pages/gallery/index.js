5beb6e4456997ab8d63f5fe4665a300a73b63fd6
David MacIntosh <macintoshdg4@gmail.com>
2021-01-12 11:51:45 +0200

    Added gallery page still trying to work out components and style

---
+++ pages/gallery/index.js
@@ -0,0 +1,32 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import styles from '../../styles/Gallery.module.css';
+
+import Navigation from '../../components/Navigation';
+
+function Gallery() {
+    return <div>
+        <Head>
+        <title>The Gallery</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Navigation></Navigation>
+      <div className={styles.banner}>
+            <h1>The Gallery</h1>
+    </div>
+    <main>
+        
+    </main>
+    <footer className={styles.footer}>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Created by{' '}
+          <img src="../../assets/ADSolutionsLogo.jpg" alt="ADSolutions Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </div>
+  }
+  
+  export default Gallery
