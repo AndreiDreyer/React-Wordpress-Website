d1afc98f73c7b68ef34fbd1cd876cc2132a119b3
Andrei Dreyer <andrei.dreyer1997@gmail.com>
2021-01-12 18:06:14 +0200

    Updating home page to use sidebar navigation

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Button from '@material-ui/core/Button';
-import Container from '@material-ui/core/Container';
+
+import Navigation from '../components/Navigation';
 
 import Head from 'next/head';
 import Link from 'next/link';
 import styles from '../styles/Home.module.css';
 
-import Navigation from '../components/Navigation';
 import { Grid } from '@material-ui/core';
 import { getMenu } from '../lib/api';
 
@@ -40,26 +37,7 @@ export default function Home({ menuItems }) {
       direction="row"
       className={classes.menuGrid}
     >
-      {/* Rotating Images */}
-      <AppBar position="fixed" className={classes.appBar}>
-        <Toolbar>
-          <Grid justify="space-between" container spacing={24}>
-            <Grid item></Grid>
-            <Grid item>
-              <Grid item>
-                <Button variant="outlined" className={classes.phoneButton}>
-                  Phone
-                </Button>
-                <Button variant="outlined">Email</Button>
-              </Grid>
-            </Grid>
-          </Grid>
-          {/* Logo */}
-        </Toolbar>
-      </AppBar>
-      <Grid justify="center">
-        <p>this is the menu</p>
-      </Grid>
+      <Navigation menuItems={menuItems} />
     </Grid>
   );
 }
