3cf5982a4e806aabc0b5887a9f2db59a130625c5
Andrei Dreyer <andrei.dreyer1997@gmail.com>
2021-01-15 18:25:39 +0200

    Working sample shop item from WooCommerce, other dummies are simply to test grid

--- pages/shop/index.js
+++ pages/shop/index.js
@@ -3,11 +3,43 @@ import Navigation from '../../components/Navigation';
 import { getMenu, getProducts } from '../../lib/api';
 
 import Head from 'next/head';
-import { Container } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
+
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
 
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles({});
+const useStyles = makeStyles({
+  productSection: {
+    display: 'flex',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+  productItem: {
+    margin: '1em 0 0 1em',
+    textAlign: 'center',
+  },
+  gridContainer: {
+    width: '100%',
+  },
+  productGrid: {
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+  mediaCard: {
+    maxWidth: 345,
+  },
+  mediaItem: {
+    height: 200,
+    objectFit: 'contain',
+  },
+});
 
 export default function Shop({ products, menuItems }) {
   const classes = useStyles();
@@ -15,24 +47,62 @@ export default function Shop({ products, menuItems }) {
   console.log(products);
 
   return (
-    <React.Fragment>
+    <div className={classes.productSection}>
       <Head>
         <title>The Salty Zebra</title>
         <link rel="icon" href="/favicon.ico"></link>
       </Head>
       <Navigation menuItems={menuItems} />
-      <Container maxWidth="lg">
-        <section>
-          {products.map((product) => {
-            return (
-              <div key={product.name}>
-                <p>Product: {product.name}</p>
-              </div>
-            );
-          })}
-        </section>
-      </Container>
-    </React.Fragment>
+      <Grid
+        container
+        spacing={0}
+        className={classes.productGrid}
+        alignItems="center"
+        justify="center"
+      >
+        {products.map((product) => {
+          console.log(product.images);
+          return (
+            <Grid item xs={12} md={5} lg={3} className={classes.productItem}>
+              <Card className={classes.mediaCard}>
+                <CardActionArea>
+                  <CardMedia
+                    className={classes.mediaItem}
+                    image={product.images[0].src}
+                    title={product.images[0].name}
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {product.name}
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      color="textSecondary"
+                      component="p"
+                      dangerouslySetInnerHTML={{
+                        __html: product.short_description,
+                      }}
+                    ></Typography>
+                    <Typography variant="h5" component="h5">
+                      ${product.price}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          );
+        })}
+        <Grid item xs={12} md={5} lg={3} className={classes.productItem}>
+          <p> Test Print 2 </p>
+        </Grid>
+        <Grid item xs={12} md={5} lg={3} className={classes.productItem}>
+          <p>Test Print 3</p>
+        </Grid>
+        <Grid item xs={12} md={5} lg={3} className={classes.productItem}>
+          <p>Test Print 4</p>
+        </Grid>
+      </Grid>
+    </div>
   );
 }
 
