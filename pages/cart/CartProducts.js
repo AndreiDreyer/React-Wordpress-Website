8fca7617a1781c7329dc46d5546ba3d72bc421dc
Andrei Dreyer <andrei.dreyer1997@gmail.com>
2021-01-21 16:18:57 +0200

    Added clearing of cart and removing specific items from cart functionality

--- pages/cart/CartProducts.js
+++ pages/cart/CartProducts.js
@@ -2,13 +2,20 @@ import React, { useContext } from 'react';
 
 import { CartContext } from '../../src/contexts/CartContext';
 
+import Button from '@material-ui/core/Button';
+
 export default function CartProducts() {
-  const { cartItems } = useContext(CartContext);
+  const { cartItems, removeProduct } = useContext(CartContext);
 
   return (
     <div>
       {cartItems.map((product) => (
-        <div>{product.name}</div>
+        <div key={product.id}>
+          <div>{product.name}</div>
+          <div>
+            <Button onClick={() => removeProduct(product)}>Remove Item</Button>
+          </div>
+        </div>
       ))}
     </div>
   );
--- pages/cart/index.js
+++ pages/cart/index.js
@@ -3,6 +3,7 @@ import { CartContext } from '../../src/contexts/CartContext';
 
 import { Link } from 'next/link';
 import CartProducts from './CartProducts';
+import Button from '@material-ui/core/Button';
 
 export default function Cart() {
   const {
@@ -31,6 +32,15 @@ export default function Cart() {
           <div>Total: {total}</div>
         ) : (
           <div>Cart is empty </div>
+        )}
+      </div>
+      <div>
+        {cartItems.length > 0 ? (
+          <div>
+            <Button onClick={() => clearCart()}>Clear Cart</Button>
+          </div>
+        ) : (
+          <div></div>
         )}
       </div>
     </div>
