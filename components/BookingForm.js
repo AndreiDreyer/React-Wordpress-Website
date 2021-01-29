89f8f08adbcce84384d3f35e10213b5fabcdcedb
Andrei Dreyer <andrei.dreyer1997@gmail.com>
2021-01-29 14:14:58 +0200

    Fixed center alignment on booking form

--- components/BookingForm.js
+++ components/BookingForm.js
@@ -1,114 +1,130 @@
-import React from 'react'
-import Swal from 'sweetalert2'
+import React, { useState } from 'react';
+import Swal from 'sweetalert2';
 
-export default function BookingForm(){
-    // constructor(props) {
-    //     super(props);
-        const state = { feedback: '',
-                      name: '',
-                      email: '' 
-                     };
-      }
-      // saves the user's name entered to state
-      const nameChange = (event) => {
-        state = setState({name: event.target.value})
-      }
-      
-      // saves the user's email entered to state
-      const emailChange = (event) => {
-        state = setState({email: event.target.value})
-      }
+import { makeStyles } from '@material-ui/core/styles';
 
-      // saves the user's message entered to state
-      const messageChange = (event) => {
-        state = setState({feedback: event.target.value})
-      }
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignContent: 'center',
+    height: '100%',
+    //   overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
 
-      //onSubmit of email form
-      const handleSubmit = (event) => {
-        event.preventDefault();
+export default function BookingForm() {
+  const classes = useStyles();
+  // constructor(props) {
+  //     super(props);
+  const state = { feedback: '', name: '', email: '' };
+  // saves the user's name entered to state
+  const nameChange = (event) => {
+    state = setState({ name: event.target.value });
+  };
 
-        //This templateId is created in EmailJS.com
-        const templateId = 'template_vz2845l';
-    
-        //This is a custom method from EmailJS that takes the information 
-        //from the form and sends the email with the information gathered 
-        //and formats the email based on the templateID provided.
-        sendFeedback(templateId, {
-                                        message: this.state.feedback, 
-                                        name: this.state.name, 
-                                        email: this.state.email
-                                       }
-                         )
+  // saves the user's email entered to state
+  const emailChange = (event) => {
+    state = setState({ email: event.target.value });
+  };
 
-      }
-    
-      //Custom EmailJS method
-      const sendFeedback = (templateId, variables) => {
-        window.emailjs.send(
-          'gmail', templateId,
-          variables
-          ).then(res => {
-            // Email successfully sent alert
-            Swal.fire({
-              title: 'Email Successfully Sent',
-              icon: 'success'
-            })
-          })
-          // Email Failed to send Error alert
-          .catch(err => {
-            Swal.fire({
-              title: 'Email Failed to Send',
-              icon: 'error'
-            })
-            console.error('Email Error:', err)
-          })
-      }
-    
-      // render() {
-        return (
-          
-          //Form layout that requires a Name, Email, and message
-          <form className="test-mailing" onSubmit={handleSubmit}>
+  // saves the user's message entered to state
+  const messageChange = (event) => {
+    state = setState({ feedback: event.target.value });
+  };
 
-            <br/>
-            <div style={{fontSize: "1.2rem"}}>
+  //onSubmit of email form
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-              <h6>You can also send me an email directly from here</h6>
-              <div>
-                  <label htmlFor="name">Name</label>
-                  <input className="form-control email-inputs" name="user_name" type="text" 
-                    id="name" onChange={nameChange} required/>
-              </div>
+    //This templateId is created in EmailJS.com
+    const templateId = 'template_vz2845l';
 
-              <div>
-                  <label htmlFor="email">Email</label>
-                  <input className="form-control email-inputs" name="user_email" type="text"
-                    id="email" onChange={emailChange} required/>
-              </div>
+    //This is a custom method from EmailJS that takes the information
+    //from the form and sends the email with the information gathered
+    //and formats the email based on the templateID provided.
+    sendFeedback(templateId, {
+      message: this.state.feedback,
+      name: this.state.name,
+      email: this.state.email,
+    });
+  };
 
-              <label htmlFor="message">
-                  Message
-              </label>
-              <div>
-                <textarea
-                  id="message"
-                  name="message"
-                  onChange={messageChange}
-                  placeholder="Put your message here"
-                  required
-                  className="email-text-area form-control"
-                  rows="15"
-                  cols="20"
-                />
-              </div>
+  //Custom EmailJS method
+  const sendFeedback = (templateId, variables) => {
+    window.emailjs
+      .send('gmail', templateId, variables)
+      .then((res) => {
+        // Email successfully sent alert
+        Swal.fire({
+          title: 'Email Successfully Sent',
+          icon: 'success',
+        });
+      })
+      // Email Failed to send Error alert
+      .catch((err) => {
+        Swal.fire({
+          title: 'Email Failed to Send',
+          icon: 'error',
+        });
+        console.error('Email Error:', err);
+      });
+  };
 
-            </div>
+  // render() {
+  return (
+    //Form layout that requires a Name, Email, and message
+    <form className={classes.root} onSubmit={handleSubmit}>
+      <br />
+      <div style={{ fontSize: '1.2rem' }}>
+        <h6>You can also send me an email directly from here</h6>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input
+            className="form-control email-inputs"
+            name="user_name"
+            type="text"
+            id="name"
+            onChange={nameChange}
+            required
+          />
+        </div>
 
-            <input type="submit" value="Submit" className="btn btn-outline-light" />
-          </form>
-        );
-      }
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            className="form-control email-inputs"
+            name="user_email"
+            type="text"
+            id="email"
+            onChange={emailChange}
+            required
+          />
+        </div>
+
+        <label htmlFor="message">Message</label>
+        <div>
+          <textarea
+            id="message"
+            name="message"
+            onChange={messageChange}
+            placeholder="Put your message here"
+            required
+            className="email-text-area form-control"
+            rows="15"
+            cols="20"
+          />
+        </div>
+      </div>
+
+      <input type="submit" value="Submit" className="btn btn-outline-light" />
+    </form>
+  );
+}
 // }
 
 // export default BookingForm
--- pages/bookings/index.js
+++ pages/bookings/index.js
@@ -5,62 +5,63 @@ import styles from '../../styles/Gallery.module.css';
 import { getMenu } from '../../lib/api';
 
 import Navigation from '../../components/Navigation';
-import Calendar from '../../components/Calendar'
+import Calendar from '../../components/Calendar';
 
-import { render } from "react-dom";
-import {momentLocalizer, Views } from "react-big-calendar";
-import moment from "moment";
-import "react-big-calendar/lib/css/react-big-calendar.css";
+import { render } from 'react-dom';
+import { momentLocalizer, Views } from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 import { makeStyles } from '@material-ui/core/styles';
 import BookingForm from '../../components/BookingForm';
-moment.locale("en-GB");
-
+moment.locale('en-GB');
 
 const localizer = momentLocalizer(moment);
 
-
 const myEventsList = [
-    {
-    'title': 'All Day Event very long title',
-    'allDay': true,
-    'start': new Date(2015, 3, 0),
-    'end': new Date(2015, 3, 1)},
-    ] //empty object for now
+  {
+    title: 'All Day Event very long title',
+    allDay: true,
+    start: new Date(2015, 3, 0),
+    end: new Date(2015, 3, 1),
+  },
+]; //empty object for now
 
-    const useStyles = makeStyles((theme) => ({
-        root: {
-          display: 'flex',
-          flexWrap: 'wrap',
-          justifyContent: 'space-around',
-          height: 980,
-        //   overflow: 'hidden',
-          backgroundColor: theme.palette.background.paper,
-        },
-      }));
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignContent: 'center',
+    height: '100%',
+    //   overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
 
-function Booking({menuItems}) {
-    const classes = useStyles();
+function Booking({ menuItems }) {
+  const classes = useStyles();
 
-    return <div>
-        <Head>
+  return (
+    <div>
+      <Head>
         <title>The Gallery</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Navigation menuItems={menuItems} ></Navigation>
-      <BookingForm
-      className={classes.root}
-      ></BookingForm>    
+      <Navigation menuItems={menuItems}></Navigation>
+      <BookingForm className={classes.root}></BookingForm>
     </div>
-  }
+  );
+}
 
-  export async function getStaticProps() {
-    const menuItems = await getMenu();
-    return {
-      props: {
-        menuItems,
-      },
-    };
-  }
-  
-  
-  export default Booking
+export async function getStaticProps() {
+  const menuItems = await getMenu();
+  return {
+    props: {
+      menuItems,
+    },
+  };
+}
+
+export default Booking;
