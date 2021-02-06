d6bf0eefc9080e6b5f9eebf544b010a04e24c622
Andrei Dreyer <andrei.dreyer1997@gmail.com>
2021-02-06 23:29:44 +0200

    Form validation on Checkout works

--- components/CheckoutForm.js
+++ components/CheckoutForm.js
@@ -1,14 +1,19 @@
-import React, { useContext } from 'react';
-import { Formik, withFormik } from 'formik';
+import React, { useContext, useState } from 'react';
+import { useRouter } from 'next/router';
+import { Formik, withFormik, useFormik } from 'formik';
 import * as Yup from 'yup';
 import TextField from '@material-ui/core/TextField';
 import MenuItem from '@material-ui/core/MenuItem';
 import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import { withStyles } from '@material-ui/core/styles';
 
 import { CartContext } from '../src/contexts/CartContext';
 
 import PaypalButton from './PaypalButtons';
+import { fetchHelper } from '../src/utils';
 
 const useStyles = () => ({
   root: {
@@ -24,169 +29,258 @@ const useStyles = () => ({
     marginTop: '1rem',
   },
 });
+
 const form = (props) => {
-  const { classes, values, touched, errors, isSubmitting, handleChange, handleBlur, handleSubmit, handleReset } = props;
+  const { classes } = props;
 
-  const { total } = useContext(CartContext);
+  const { total, cartItems } = useContext(CartContext);
 
-  const onSuccess = () => {};
+  const [processing, setProcessing] = useState(false);
+
+  const router = useRouter();
+
+  const onSuccess = async () => {
+    const customerData = {
+      first_name: values.firstName,
+      last_name: values.surname,
+      address_1: values.address,
+      address_2: '',
+      city: values.city,
+      state: values.province,
+      postcode: '0000',
+      country: values.country,
+      email: values.email,
+      phone: values.phoneNumber,
+    };
+
+    const lineItems = cartItems.map((item) => ({ product_id: item.product_id, variation_id: item.id, quantity: item.quantity }));
+
+    const data = {
+      payment_method: 'paypal',
+      payment_method_title: 'PayPal Standard',
+      set_paid: true,
+      billing: customerData,
+      shipping: customerData,
+      line_items: lineItems,
+      shipping_lines: [],
+    };
+
+    try {
+      const res = await fetchHelper('./api/createOrder', 'POST', data);
+
+      if (res.status === 200 || res.status === 201) {
+        router.push('/checkout/success');
+      } else {
+        router.push('/checkout/failure');
+      }
+    } catch (err) {
+      console.log('Error Frontend: ', err);
+    } finally {
+      setProcessing(false);
+    }
+  };
+
+  const onProcessing = () => {
+    setProcessing(true);
+  };
+  const { values, touched, errors, isSubmitting, handleChange, handleBlur, handleSubmit, handleReset, isValid, dirty } = useFormik({
+    initialValues: {
+      firstName: '',
+      surname: '',
+      addressLine1: '',
+      city: '',
+      province: '',
+      country: '',
+      email: '',
+      phoneNumber: '',
+    },
+
+    validationSchema: Yup.object().shape({
+      firstName: Yup.string().required('Required'),
+      surname: Yup.string().required('Required'),
+      addressLine1: Yup.string().required('Required'),
+      city: Yup.string().required('Required'),
+      province: Yup.string().required('Required'),
+      country: Yup.string().required('Required'),
+      email: Yup.string().email('Enter a valid Email').required('Required'),
+      phoneNumber: Yup.number().required('Required'),
+    }),
+
+    onSubmit: async (values, { setSubmitting }) => {
+      setProcessing(true);
+      const customerData = {
+        first_name: values.firstName,
+        last_name: values.surname,
+        address_1: values.address,
+        address_2: '',
+        city: values.city,
+        state: values.province,
+        postcode: '0000',
+        country: values.country,
+        email: values.email,
+        phone: values.phoneNumber,
+      };
+
+      const lineItems = cartItems.map((item) => ({ product_id: item.product_id, variation_id: item.id, quantity: item.quantity }));
+
+      const data = {
+        payment_method: 'cod',
+        payment_method_title: 'Cash on delivery',
+        set_paid: false,
+        billing: customerData,
+        shipping: customerData,
+        line_items: lineItems,
+        shipping_lines: [],
+      };
+
+      try {
+        const res = await fetchHelper('./api/createOrder', 'POST', data);
+
+        if (res.status === 200 || res.status === 201) {
+          router.push('/checkout/success');
+        } else {
+          router.push('/checkout/failure');
+        }
+      } catch (err) {
+        console.log('Error Frontend: ', err);
+      } finally {
+        setProcessing(false);
+      }
+    },
+  });
 
   return (
-    <form onSubmit={handleSubmit}>
-      <TextField
-        required
-        id="firstName"
-        label="First Name"
-        variant="standard"
-        name="firstName"
-        className={classes.textField}
-        value={values.firstName}
-        onChange={handleChange}
-        onBlur={handleBlur}
-        helperText={touched.firstName ? errors.firstName : ''}
-        error={touched.firstName && Boolean(errors.firstName)}
-      />
-      <TextField
-        required
-        id="surname"
-        label="Surname"
-        variant="standard"
-        name="surname"
-        className={classes.textField}
-        value={values.surname}
-        onChange={handleChange}
-        onBlur={handleBlur}
-        helperText={touched.surname ? errors.surname : ''}
-        error={touched.surname && Boolean(errors.surname)}
-      />
-      <TextField
-        required
-        id="addressLine1"
-        label="Address Line 1"
-        variant="standard"
-        name="addressLine1"
-        className={classes.textField}
-        value={values.address}
-        onChange={handleChange}
-        onBlur={handleBlur}
-        helperText={touched.addressLine1 ? errors.addressLine1 : ''}
-        error={touched.addressLine1 && Boolean(errors.addressLine1)}
-      />
-      <TextField
-        required
-        id="city"
-        label="City"
-        variant="standard"
-        name="city"
-        className={classes.textField}
-        value={values.city}
-        onChange={handleChange}
-        onBlur={handleBlur}
-        helperText={touched.city ? errors.city : ''}
-        error={touched.city && Boolean(errors.city)}
-      />
-      <TextField
-        required
-        id="province"
-        label="Province"
-        variant="standard"
-        name="province"
-        className={classes.textField}
-        value={values.province}
-        onChange={handleChange}
-        onBlur={handleBlur}
-        helperText={touched.province ? errors.province : ''}
-        error={touched.province && Boolean(errors.province)}
-      />
-      <TextField
-        required
-        select
-        id="country"
-        label="Country"
-        variant="standard"
-        name="country"
-        className={classes.textField}
-        value={values.country}
-        onChange={handleChange}
-        onBlur={handleBlur}
-        helperText={touched.country ? errors.country : ''}
-        error={touched.country && Boolean(errors.country)}
-      >
-        <MenuItem value="ZIM">Zimbabwe</MenuItem>
-      </TextField>
-      <TextField
-        required
-        id="email"
-        label="Email Address"
-        type="email"
-        variant="standard"
-        name="email"
-        className={classes.textField}
-        value={values.email}
-        onChange={handleChange}
-        onBlur={handleBlur}
-        helperText={touched.email ? errors.email : ''}
-        error={touched.email && Boolean(errors.email)}
-      />
-      <TextField
-        required
-        id="phoneNumber"
-        label="Phone Number"
-        variant="standard"
-        name="phoneNumber"
-        className={classes.textField}
-        value={values.phoneNumber}
-        onChange={handleChange}
-        onBlur={handleBlur}
-        helperText={touched.phoneNumber ? errors.phoneNumber : ''}
-        error={touched.phoneNumber && Boolean(errors.phoneNumber)}
-      />
-      {Form.isValid ? (
-        <div>
-          <p>Valid Form</p>
-        </div>
-      ) : (
-        <div>
-          <p>Invalid Form</p>
-        </div>
-      )}
-      <Button type="submit" color="primary">
-        Checkout
-      </Button>
-      <PaypalButton paymentAmount={total}></PaypalButton>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          required
+          id="firstName"
+          label="First Name"
+          variant="standard"
+          name="firstName"
+          className={classes.textField}
+          value={values.firstName}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          helperText={touched.firstName ? errors.firstName : ''}
+          error={touched.firstName && Boolean(errors.firstName)}
+        />
+        <TextField
+          required
+          id="surname"
+          label="Surname"
+          variant="standard"
+          name="surname"
+          className={classes.textField}
+          value={values.surname}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          helperText={touched.surname ? errors.surname : ''}
+          error={touched.surname && Boolean(errors.surname)}
+        />
+        <TextField
+          required
+          id="addressLine1"
+          label="Address Line 1"
+          variant="standard"
+          name="addressLine1"
+          className={classes.textField}
+          value={values.address}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          helperText={touched.addressLine1 ? errors.addressLine1 : ''}
+          error={touched.addressLine1 && Boolean(errors.addressLine1)}
+        />
+        <TextField
+          required
+          id="city"
+          label="City"
+          variant="standard"
+          name="city"
+          className={classes.textField}
+          value={values.city}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          helperText={touched.city ? errors.city : ''}
+          error={touched.city && Boolean(errors.city)}
+        />
+        <TextField
+          required
+          id="province"
+          label="Province"
+          variant="standard"
+          name="province"
+          className={classes.textField}
+          value={values.province}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          helperText={touched.province ? errors.province : ''}
+          error={touched.province && Boolean(errors.province)}
+        />
+        <TextField
+          required
+          select
+          id="country"
+          label="Country"
+          variant="standard"
+          name="country"
+          className={classes.textField}
+          value={values.country}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          helperText={touched.country ? errors.country : ''}
+          error={touched.country && Boolean(errors.country)}
+        >
+          <MenuItem value="ZIM">Zimbabwe</MenuItem>
+        </TextField>
+        <TextField
+          required
+          id="email"
+          label="Email Address"
+          type="email"
+          variant="standard"
+          name="email"
+          className={classes.textField}
+          value={values.email}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          helperText={touched.email ? errors.email : ''}
+          error={touched.email && Boolean(errors.email)}
+        />
+        <TextField
+          required
+          id="phoneNumber"
+          label="Phone Number"
+          variant="standard"
+          name="phoneNumber"
+          className={classes.textField}
+          value={values.phoneNumber}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          helperText={touched.phoneNumber ? errors.phoneNumber : ''}
+          error={touched.phoneNumber && Boolean(errors.phoneNumber)}
+        />
+        {isValid && dirty ? (
+          <div>
+            <Button type="submit" color="primary">
+              Checkout
+            </Button>
+            <PaypalButton paymentAmount={total} onSuccess={onSuccess} onProcess={onProcessing}></PaypalButton>
+          </div>
+        ) : (
+          <div>
+            <p>Please complete form to show payment options</p>
+          </div>
+        )}
+      </form>
+      <Dialog open={processing}>
+        <DialogTitle id="simple-dialog-title">Processing Order</DialogTitle>
+        <CircularProgress />
+      </Dialog>
+    </div>
   );
 };
 
-const Form = withFormik({
-  mapPropsToValues: ({ firstName, surname, addressLine1, city, province, country, email, phoneNumber }) => {
-    return {
-      firstName: firstName || '',
-      surname: surname || '',
-      addressLine1: addressLine1 || '',
-      city: city || '',
-      province: province || '',
-      country: country || '',
-      email: email || '',
-      phoneNumber: phoneNumber || '',
-    };
-  },
+async function createOrder(values) {}
 
-  validationSchema: Yup.object().shape({
-    firstName: Yup.string().required('Required'),
-    surname: Yup.string().required('Required'),
-    addressLine1: Yup.string().required('Required'),
-    city: Yup.string().required('Required'),
-    province: Yup.string().required('Required'),
-    country: Yup.string().required('Required'),
-    email: Yup.string().email('Enter a valid Email').required('Required'),
-    phoneNumber: Yup.number().required('Required'),
-  }),
-
-  handleSubmit: (values, { setSubmitting }) => {
-    console.log('Submitting');
-    console.log(values);
-  },
-})(form);
-
-export default withStyles(useStyles)(Form);
+export default withStyles(useStyles)(form);
