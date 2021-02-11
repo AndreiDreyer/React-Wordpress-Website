66f017aaeca09c0ff0ad4e358388cc324d282ef5
Andrei Dreyer <andrei.dreyer1997@gmail.com>
2021-02-11 13:54:25 +0200

    Shop grid layout complete

--- pages/shop/index.js
+++ pages/shop/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Navigation from '../../components/Navigation';
 import { getMenu, getProducts } from '../../lib/api';
+import TopNavBar from '../../components/TopNavbar';
 
 import Link from 'next/link';
-
 import Head from 'next/head';
-import { Grid } from '@material-ui/core';
 
+import { Grid } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
@@ -17,15 +17,24 @@ import Typography from '@material-ui/core/Typography';
 
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   productSection: {
     display: 'flex',
     marginLeft: 'auto',
     marginRight: 'auto',
+    marginTop: '75px',
   },
   productItem: {
-    margin: '1em 0 0 1em',
+    margin: '1rem 3rem 4rem 3rem',
     textAlign: 'center',
+    [theme.breakpoints.down(1024)]: {
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+  },
+  unEventProductItem: {
+    marginLeft: 0,
+    marginRight: 'auto',
   },
   gridContainer: {
     width: '100%',
@@ -34,14 +43,37 @@ const useStyles = makeStyles({
     marginLeft: 'auto',
     marginRight: 'auto',
   },
+  someGrid: {
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    justifyContent: 'space-between',
+    width: '85%',
+    [theme.breakpoints.down(1025)]: {
+      justifyContent: 'center',
+    },
+  },
   mediaCard: {
-    maxWidth: 345,
+    maxWidth: 375,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: 'auto',
+      marginRight: '0',
+    },
+    [theme.breakpoints.up('lg')]: {
+      maxWidth: 500,
+      maxHeight: 500,
+    },
   },
   mediaItem: {
     height: 200,
     objectFit: 'contain',
   },
-});
+  cardButton: {
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+}));
 
 export default function Shop({ products, menuItems }) {
   const classes = useStyles();
@@ -52,62 +84,40 @@ export default function Shop({ products, menuItems }) {
         <title>The Salty Zebra</title>
         <link rel="icon" href="/favicon.ico"></link>
       </Head>
+      <TopNavBar />
       <Navigation menuItems={menuItems} />
-      <Grid
-        container
-        spacing={0}
-        className={classes.productGrid}
-        alignItems="center"
-        justify="center"
-      >
-        {products.map((product) => {
-          console.log(product.images);
-          return (
-            <Grid
-              item
-              xs={12}
-              md={5}
-              lg={3}
-              className={classes.productItem}
-              key={product.id}
-            >
-              <Link href={`/shop/${product.id}?slug=${product.slug}`}>
-                <Card className={classes.mediaCard}>
-                  <CardActionArea>
-                    <CardMedia
-                      className={classes.mediaItem}
-                      image={product.images[0].src}
-                      title={product.images[0].name}
-                    />
-                    <CardContent>
-                      <Typography gutterBottom variant="h5" component="h2">
-                        {product.name}
-                      </Typography>
-                      <div>
-                        <Typography variant="h5" component="h5">
-                          ${product.price}
-                        </Typography>
-                      </div>
-                    </CardContent>
-                  </CardActionArea>
-                  <CardActions>
-                    <Link href={`/shop/${product.id}?slug=${product.slug}`}>
-                      <Button variant="outlined">Learn More</Button>
-                    </Link>
-                  </CardActions>
-                </Card>
-              </Link>
-            </Grid>
-          );
-        })}
-        <Grid item xs={12} md={5} lg={3} className={classes.productItem}>
-          <p> Test Print 2 </p>
-        </Grid>
-        <Grid item xs={12} md={5} lg={3} className={classes.productItem}>
-          <p>Test Print 3</p>
-        </Grid>
-        <Grid item xs={12} md={5} lg={3} className={classes.productItem}>
-          <p>Test Print 4</p>
+      <Grid container spacing={6} className={classes.productGrid} alignItems="center" justify="space-between">
+        <Grid item xs={12} className={classes.productItem}>
+          <Grid container spacing={1} className={classes.someGrid}>
+            {products.map((product) => {
+              console.log(product);
+              return product.stock_quantity > -10 ? (
+                <Grid item xs={12} md={6} lg={3} className={classes.productItem} key={product.id}>
+                  <Link href={`/shop/${product.id}?slug=${product.slug}`}>
+                    <Card className={classes.mediaCard}>
+                      <CardActionArea>
+                        <CardMedia className={classes.mediaItem} image={product.images[0].src} title={product.images[0].name} />
+                        <CardContent>
+                          <Typography gutterBottom variant="h5" component="h2">
+                            {product.name}
+                          </Typography>
+                        </CardContent>
+                      </CardActionArea>
+                      <CardActions>
+                        <Link href={`/shop/${product.id}?slug=${product.slug}`}>
+                          <Button variant="outlined" className={classes.cardButton}>
+                            View Product
+                          </Button>
+                        </Link>
+                      </CardActions>
+                    </Card>
+                  </Link>
+                </Grid>
+              ) : (
+                <div></div>
+              );
+            })}
+          </Grid>
         </Grid>
       </Grid>
     </div>
