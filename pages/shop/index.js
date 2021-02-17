4a0efe0dc193684039519efe66a7331560c63aba
Andrei Dreyer <19809301@sun.ac.za>
2021-02-17 09:25:37 +0200

    Product Page Styling (#14)
    Added styling for product page

--- pages/shop/index.js
+++ pages/shop/index.js
@@ -184,6 +184,7 @@ export default function Shop({ products, menuItems }) {
               </div>
               <Carousel autoPlay={false} interval={2000} className={classes.promotionContainer}>
                 {products.map((product) => {
+                  console.log(product);
                   if (product.featured) {
                     return (
                       <div className={classes.promotionItem}>
@@ -215,7 +216,6 @@ export default function Shop({ products, menuItems }) {
               </div>
             </GridListTile>
             {products.map((product) => {
-              console.log(product.images);
               if (!product.featured) {
                 return (
                   <GridListTile key={product.id}>
@@ -236,7 +236,7 @@ export default function Shop({ products, menuItems }) {
                 {products.map((product) => {
                   if (product.featured) {
                     return (
-                      <div>
+                      <div key={product.id}>
                         <h2>{product.name}</h2>
                         <Grid container alignItems="center" justify="center" className={classes.videoInfo}>
                           <div className={classes.videoLocation}>
@@ -325,11 +325,6 @@ function getPriceRange(products) {
         shortDescription = m[1];
       }
 
-      console.log('Description: ', description);
-      console.log('Date: ', date);
-      console.log('Location: ', location);
-      console.log('Short Description: ', shortDescription);
-
       return {
         ...product,
         videoId: videoId,
@@ -349,8 +344,6 @@ function getPriceRange(products) {
       priceRange: priceRange,
     };
   });
-
-  // console.log(rangeProducts);
 
   return rangeProducts;
 }
