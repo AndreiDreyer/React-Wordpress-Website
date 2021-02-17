788590d7eb7c4fe44cccce75517cd4a58ca927c1
Andrei Dreyer <andrei.dreyer1997@gmail.com>
2021-02-17 07:25:41 +0200

    Removed shadow because my sister said im retarded

--- pages/shop/[pid].js
+++ pages/shop/[pid].js
@@ -40,7 +40,6 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: '1',
     flexWrap: 'wrap',
     '& img': {
-      boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
       width: '60%',
       [theme.breakpoints.down(768)]: {
         width: '100%',
