19be1a8a4ad336e59e6eba3534c6a6ff25f2adf5
David MacIntosh <macintoshdg4@gmail.com>
2021-02-19 10:00:41 +0200

    Started styling individual posts

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -2,13 +2,78 @@ import { useRouter } from 'next/router';
 import Head from 'next/head';
 import Link from 'next/link';
 
-import { getAllPostsWithSlug, getPost } from '../../lib/api';
+import { getAllPostsWithSlug, getPost, getAllPosts } from '../../lib/api';
 
 import styles from '../../styles/Home.module.css';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
 import blogStyles from '../../styles/Blog.module.css';
 
-export default function Post({ postData }) {
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+
+import TopNavBar from '../../components/TopNavbar';
+
+import {
+  FiCard,
+  FiCardActionArea,
+  FiCardActions,
+  FiCardContent,
+  FiCardMedia
+} from "../../components/ImageCard";
+
+const useStyles = makeStyles((theme) => ({
+  topBanner: {
+    height: '100%',
+  },
+  bannerDiv: {
+    flexGrow: 1,
+    width: '100%',
+    [theme.breakpoints.down("sm")]: {
+      height: 215,
+      marginBottom: 86, 
+    },
+    [theme.breakpoints.up("1024")]: {
+      height: 215,
+      marginBottom: 86, 
+      marginLeft: 200,
+    },
+    [theme.breakpoints.up("lg")]: {
+      height: 215,
+      marginBottom: 86, 
+    },
+    [theme.breakpoints.up("xl")]: {
+      height: 215,
+      marginBottom: 86, 
+    },
+  },
+  heading: {
+    textAlign: 'center',
+    fontFamily: 'Whitefeather',
+    [theme.breakpoints.down("376")]: {
+      fontSize: '50px !important'
+    },
+    [theme.breakpoints.down("420")]: {
+      fontSize: 60,
+    },
+    [theme.breakpoints.up("760")]: {
+      fontSize: 100,
+    },
+    [theme.breakpoints.up("lg")]: {
+      fontSize: 200,
+    },
+  },
+  card: {
+    height: 300,
+    marginBottom: 1,
+  },
+  media: {
+    height: '100%',
+  },
+}));
+
+export default function Post({ postData, allPosts }) {
   const router = useRouter();
+  const classes = useStyles();
 
   if (!router.isFallback && !postData?.slug) {
     return <p>hmmm....looks like an error</p>;
@@ -28,8 +93,29 @@ export default function Post({ postData }) {
         <title>{postData.title}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <TopNavBar></TopNavBar>
+      {allPosts.edges.map(({ node }) => (
       <main className={styles.main}>
+      <div className={classes.bannerDiv}>
+        <Box style={{ margin: 0}}>
+          <FiCard className={classes.card}>
+            <FiCardActionArea className={classes.card}>
+              <FiCardMedia
+                media="picture"
+                alt="Contemplative Reptile"
+                image={node.extraPostInfo.thumbImage.mediaItemUrl}
+                title="Contemplative Reptile"
+                className={classes.media}
+              />
+              <FiCardContent className={classes.fiCardContent}>
+                <Typography className={classes.heading} variant="h5" component="h2">
+                Ramblings While We Roam
+                </Typography>
+              </FiCardContent>
+            </FiCardActionArea>
+          </FiCard>
+        </Box>
+      </div>
         {router.isFallback ? (
           <h2>Loading...</h2>
         ) : (
@@ -50,6 +136,7 @@ export default function Post({ postData }) {
           </Link>
         </p>
       </main>
+      ))}
     </div>
   );
 }
@@ -65,10 +152,12 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params }) {
   const data = await getPost(params.slug);
+  const allPosts = await getAllPosts();
 
   return {
     props: {
       postData: data.post,
+      allPosts,
     },
   };
 }
