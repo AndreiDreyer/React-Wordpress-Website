187ab87f42e46e06d50d47957e2c46d602d766fa
Andrei Dreyer <19809301@sun.ac.za>
2021-02-20 18:27:37 +0200

    Footer Component (#34)
    * Moved footer to seperate component
    * Added component to all the pages

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navigation from '../../components/Navigation';
 import TopNavBar from '../../components/TopNavbar';
+import Footer from '../../components/Footer';
 
 import Head from 'next/head';
 
@@ -11,8 +12,8 @@ import { makeStyles, useTheme } from '@material-ui/core/styles';
 
 import Paper from '@material-ui/core/Paper';
 
-import Typography from "@material-ui/core/Typography";
-import Box from "@material-ui/core/Box";
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import GridList from '@material-ui/core/GridList';
@@ -20,13 +21,7 @@ import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
 
 // --- Fill Image Card Component Imports --- //
-import {
-  FiCard,
-  FiCardActionArea,
-  FiCardActions,
-  FiCardContent,
-  FiCardMedia
-} from "../../components/ImageCard";
+import { FiCard, FiCardActionArea, FiCardActions, FiCardContent, FiCardMedia } from '../../components/ImageCard';
 
 const useStyles = makeStyles((theme) => ({
   // appBar: {
@@ -36,14 +31,25 @@ const useStyles = makeStyles((theme) => ({
   phoneButton: {
     marginRight: 10,
   },
+  mainContainer: {
+    [theme.breakpoints.down(765)]: {
+      position: 'relative',
+    },
+  },
+  contentWrap: {
+    paddingBottom: '75px',
+  },
   menuGrid: {
     display: 'flex',
     width: '100%',
-    [theme.breakpoints.up("lg")]: {
+    [theme.breakpoints.up('lg')]: {
       height: '100vh',
     },
-    [theme.breakpoints.up("xl")]: {
+    [theme.breakpoints.up('xl')]: {
       height: '100vh',
+    },
+    [theme.breakpoints.down(765)]: {
+      paddingBottom: '75px',
     },
   },
   rotatingImages: {
@@ -55,18 +61,18 @@ const useStyles = makeStyles((theme) => ({
   bannerDiv: {
     flexGrow: 1,
     width: '100%',
-    [theme.breakpoints.up("1024")]: {
+    [theme.breakpoints.up('1024')]: {
       height: 215,
-      marginBottom: 86, 
+      marginBottom: 86,
       marginLeft: 200,
     },
-    [theme.breakpoints.up("lg")]: {
+    [theme.breakpoints.up('lg')]: {
       height: 215,
-      marginBottom: 86, 
+      marginBottom: 86,
     },
-    [theme.breakpoints.up("xl")]: {
+    [theme.breakpoints.up('xl')]: {
       height: 215,
-      marginBottom: 86, 
+      marginBottom: 86,
     },
   },
   card: {
@@ -74,21 +80,21 @@ const useStyles = makeStyles((theme) => ({
     marginBottom: 1,
   },
   media: {
-    height: 140
+    height: 140,
   },
   heading: {
     textAlign: 'center',
     fontFamily: 'Whitefeather',
-    [theme.breakpoints.down("376")]: {
-      fontSize: '50px !important'
+    [theme.breakpoints.down('376')]: {
+      fontSize: '50px !important',
     },
-    [theme.breakpoints.down("420")]: {
+    [theme.breakpoints.down('420')]: {
       fontSize: 60,
     },
-    [theme.breakpoints.up("760")]: {
+    [theme.breakpoints.up('760')]: {
       fontSize: 100,
     },
-    [theme.breakpoints.up("lg")]: {
+    [theme.breakpoints.up('lg')]: {
       fontSize: 200,
     },
   },
@@ -96,10 +102,10 @@ const useStyles = makeStyles((theme) => ({
     marginTop: 1,
     position: 'relative',
     marginBottom: 1,
-    [theme.breakpoints.up("md")]: {
+    [theme.breakpoints.up('md')]: {
       height: '20%',
     },
-    [theme.breakpoints.up("1024")]: {
+    [theme.breakpoints.up('1024')]: {
       height: '15%',
       marginLeft: 200,
     },
@@ -110,15 +116,15 @@ const useStyles = makeStyles((theme) => ({
     display: 'flex',
     flexWrap: 'wrap',
     width: '100%',
-    
-    [theme.breakpoints.up("1024")]: {
+
+    [theme.breakpoints.up('1024')]: {
       height: '100%',
       '& > *': {
         marginLeft: theme.spacing(4),
         marginRight: theme.spacing(4),
       },
     },
-    [theme.breakpoints.down("md")]: {
+    [theme.breakpoints.down('md')]: {
       height: '100%',
     },
   },
@@ -126,7 +132,7 @@ const useStyles = makeStyles((theme) => ({
     marginTop: 1,
     position: 'relative',
     height: '60%',
-    [theme.breakpoints.up("1024")]: {
+    [theme.breakpoints.up('1024')]: {
       marginLeft: 200,
     },
   },
@@ -159,18 +165,21 @@ const useStyles = makeStyles((theme) => ({
   grid: {
     flexGrow: 1,
     width: '100%',
-    [theme.breakpoints.up("lg")]: {
+    [theme.breakpoints.up('lg')]: {
       marginLeft: 200,
     },
   },
   gridList: {
-    flexWrap: 'nowrap'
+    flexWrap: 'nowrap',
   },
   tile: {
     height: '100%',
   },
+  footer: {
+    position: 'absolute',
+    bottom: 0,
+  },
 }));
-
 
 export default function About({ menuItems }) {
   const classes = useStyles();
@@ -179,17 +188,17 @@ export default function About({ menuItems }) {
   const tileData = [
     {
       img: './warthog1.jpg',
-      title: 'title'
+      title: 'title',
     },
     {
       img: './Gin1.jpg',
-      title: 'title'
+      title: 'title',
     },
     {
       img: './RR1.jpg',
-      title: 'title'
-    }
-    ];
+      title: 'title',
+    },
+  ];
 
   const sm = useMediaQuery(theme.breakpoints.down('sm'));
   const md = useMediaQuery(theme.breakpoints.down('md'));
@@ -212,70 +221,82 @@ export default function About({ menuItems }) {
   };
 
   return (
-    <Grid container spacing={0} direction="row" className={classes.menuGrid}>
-      <Head>
-        <title>About Us</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <TopNavBar></TopNavBar>
-      <Navigation menuItems={menuItems} />
-      <div className={classes.bannerDiv}>
-        <Box style={{ margin: 0}}>
-          <FiCard className={classes.card}>
-            <FiCardActionArea className={classes.card}>
-              <FiCardMedia
-                media="picture"
-                alt="Contemplative Reptile"
-                image='./_MG_9966.jpg'
-                title="Contemplative Reptile"
-              />
-              <FiCardContent className={classes.fiCardContent}>
-                <Typography className={classes.heading} variant="h5" component="h2">
-                  Making Memories Matter
-                </Typography>
-              </FiCardContent>
-            </FiCardActionArea>
-          </FiCard>
-        </Box>
-      </div>
-      
-      <div className={classes.paperDiv}>
-        <Paper className={classes.paper} variant='outlined'>
-          <Typography className={classes.writing} variant="h5" component="h2">
-          One cannot deny, Zimbabwe is a country of contrasts. Those that have ventured here will be extremely familiar with the peculiar set of rules attached to this lands governance. And thus, in keeping with this particularly absurd tradition, the Salty Zebra was conceived of a contrast. You see, the founder of this collective is both firmly rooted in her Zimbabwean heritage, while also having a fair amount of sea foam in her veins and with both a degree in Visual Communication and a MSDT Dive Instructor Rating, it seemed the only logical thing when placing a name on this collective, would be to contradict all logic and create a fair amount of contradiction. Meet the Salty Zebra Creative Company, equally salty and perfectly dusty, we are a team with a passion for making memories matter. For searching through the multitudes of moments for that one golden opportunity to capture something seeped in emotion and packed with meaning. 
-          </Typography>
-        </Paper>
-      </div>
-      
-      <div className={classes.grid}>
-        <GridList className={classes.gridList} cols={numCols()} rows={2}>
-          {tileData.map((tile) => (
-            <GridListTile className={classes.tile} rows={2} key={tile.img}>
-              <img src={tile.img} alt={tile.title} />            
-            </GridListTile>
-          ))}
-        </GridList>
-      </div>
+    <div className={classes.mainContainer}>
+      <Grid container spacing={0} direction="row" className={classes.menuGrid}>
+        <Head>
+          <title>About Us</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <TopNavBar></TopNavBar>
+        <Navigation menuItems={menuItems} />
+        <div className={classes.bannerDiv}>
+          <Box style={{ margin: 0 }}>
+            <FiCard className={classes.card}>
+              <FiCardActionArea className={classes.card}>
+                <FiCardMedia media="picture" alt="Contemplative Reptile" image="./_MG_9966.jpg" title="Contemplative Reptile" />
+                <FiCardContent className={classes.fiCardContent}>
+                  <Typography className={classes.heading} variant="h5" component="h2">
+                    Making Memories Matter
+                  </Typography>
+                </FiCardContent>
+              </FiCardActionArea>
+            </FiCard>
+          </Box>
+        </div>
 
-      <div className={classes.paperDiv2}>
-        <Paper className={classes.paper2} variant='outlined'>
-          <Typography className={classes.writingHeading} variant="h5" component="h2">
-            What we do.  
-          </Typography>
-          <Typography className={classes.writing} variant="h5" component="h2">
-          Making memories matter, is what we’re into. We tend to have a bit of a rebellious streak, and rather enjoy bending and pausing time, even just for a moment, and if only for a split second long enough for the shutter to close. Growing up in a country well rehearsed in things not quite going according to the plan, we are adept in capturing memories as they happen and are not phased with having to go the extra mile to get the perfect shot. With Jessie trained in stills but unable to help herself with recording a moving memory every now and then, and Chad working every angle from both the ground and the air, they are the perfect pair to make your memories matter ! 
-          </Typography>
-          <Typography className={classes.writingHeading} variant="h5" component="h2">
-            Who we are.  
-          </Typography>
-          <Typography className={classes.writing} variant="h5" component="h2">
-          While we were growing up our dad had a butterfly collection. It used to sit proudly in the corner of the lounge, a deep mahogany with what seemed like hundreds of drawers. There were realistically about twenty of them, and each stored a childhood of memories. For within those pinioned wings and perfectly preserved forms lay a reminder of an adventure, a memory of the times we spent paddling through the Zambezi river, our motor had a tendency of being rather temperamental, or sweating our way through the harsh lowveld of West Nicholson or braving our way through the waters of Mozambique. We never realized it at the time, but our father was instilling in us the art of observation. Of looking through the normality of the world, and finding those intricately patterned, vividly colored fleetingly pervasive, moments. Moments that when you turn a lens on them, we’ve found, tend to become magic. 
-          Using their homeland, Zimbabwe, as their muse, and the almost instinctual ability to ‘make a plan’ that tends to be instilled in most Zimbabweans, brother-sister team Jessie and Chad have taken those skills instilled by their dad, honed them in two of the top creative institutions in Southern Africa, and are now adding their own flare to what they are dubbing ‘Brett’s Butterfly Effect’ 
-          </Typography>
-        </Paper>
-      </div>
-    </Grid>
+        <div className={classes.paperDiv}>
+          <Paper className={classes.paper} variant="outlined">
+            <Typography className={classes.writing} variant="h5" component="h2">
+              One cannot deny, Zimbabwe is a country of contrasts. Those that have ventured here will be extremely familiar with the peculiar set of rules attached to this lands
+              governance. And thus, in keeping with this particularly absurd tradition, the Salty Zebra was conceived of a contrast. You see, the founder of this collective is both
+              firmly rooted in her Zimbabwean heritage, while also having a fair amount of sea foam in her veins and with both a degree in Visual Communication and a MSDT Dive
+              Instructor Rating, it seemed the only logical thing when placing a name on this collective, would be to contradict all logic and create a fair amount of
+              contradiction. Meet the Salty Zebra Creative Company, equally salty and perfectly dusty, we are a team with a passion for making memories matter. For searching
+              through the multitudes of moments for that one golden opportunity to capture something seeped in emotion and packed with meaning.
+            </Typography>
+          </Paper>
+        </div>
 
+        <div className={classes.grid}>
+          <GridList className={classes.gridList} cols={numCols()} rows={2}>
+            {tileData.map((tile) => (
+              <GridListTile className={classes.tile} rows={2} key={tile.img}>
+                <img src={tile.img} alt={tile.title} />
+              </GridListTile>
+            ))}
+          </GridList>
+        </div>
+
+        <div className={classes.paperDiv2}>
+          <Paper className={classes.paper2} variant="outlined">
+            <Typography className={classes.writingHeading} variant="h5" component="h2">
+              What we do.
+            </Typography>
+            <Typography className={classes.writing} variant="h5" component="h2">
+              Making memories matter, is what we’re into. We tend to have a bit of a rebellious streak, and rather enjoy bending and pausing time, even just for a moment, and if
+              only for a split second long enough for the shutter to close. Growing up in a country well rehearsed in things not quite going according to the plan, we are adept in
+              capturing memories as they happen and are not phased with having to go the extra mile to get the perfect shot. With Jessie trained in stills but unable to help
+              herself with recording a moving memory every now and then, and Chad working every angle from both the ground and the air, they are the perfect pair to make your
+              memories matter !
+            </Typography>
+            <Typography className={classes.writingHeading} variant="h5" component="h2">
+              Who we are.
+            </Typography>
+            <Typography className={classes.writing} variant="h5" component="h2">
+              While we were growing up our dad had a butterfly collection. It used to sit proudly in the corner of the lounge, a deep mahogany with what seemed like hundreds of
+              drawers. There were realistically about twenty of them, and each stored a childhood of memories. For within those pinioned wings and perfectly preserved forms lay a
+              reminder of an adventure, a memory of the times we spent paddling through the Zambezi river, our motor had a tendency of being rather temperamental, or sweating our
+              way through the harsh lowveld of West Nicholson or braving our way through the waters of Mozambique. We never realized it at the time, but our father was instilling
+              in us the art of observation. Of looking through the normality of the world, and finding those intricately patterned, vividly colored fleetingly pervasive, moments.
+              Moments that when you turn a lens on them, we’ve found, tend to become magic. Using their homeland, Zimbabwe, as their muse, and the almost instinctual ability to
+              ‘make a plan’ that tends to be instilled in most Zimbabweans, brother-sister team Jessie and Chad have taken those skills instilled by their dad, honed them in two of
+              the top creative institutions in Southern Africa, and are now adding their own flare to what they are dubbing ‘Brett’s Butterfly Effect’
+            </Typography>
+          </Paper>
+        </div>
+      </Grid>
+      <Footer />
+    </div>
   );
 }
 
--- pages/checkout/index.js
+++ pages/checkout/index.js
@@ -2,28 +2,33 @@ import React, { useContext, useState } from 'react';
 
 import { CartContext } from '../../src/contexts/CartContext';
 import { getMenu } from '../../lib/api';
-import { fetchHelper } from '../../src/utils';
 import Form from '../../components/CheckoutForm';
 
 import { makeStyles } from '@material-ui/core/styles';
 
-import { Link } from 'next/link';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import { useRouter } from 'next/router';
 
 import Navigation from '../../components/Navigation';
 import TopNavBar from '../../components/TopNavbar';
+import Footer from '../../components/Footer';
 
 import Container from '@material-ui/core/Container';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-import { PlayCircleFilledWhite } from '@material-ui/icons';
 
 const useStyles = makeStyles((theme) => ({
   root: {
+    [theme.breakpoints.down(765)]: {
+      position: 'relative',
+    },
     display: 'flex',
     flexWrap: 'wrap',
+  },
+  contentWrap: {
+    [theme.breakpoints.down(765)]: {
+      paddingBottom: '75px',
+    },
   },
   textField: {
     marginLeft: theme.spacing(1),
@@ -33,31 +38,27 @@ const useStyles = makeStyles((theme) => ({
   gridItem: {
     marginBottom: theme.spacing(1),
     marginTop: theme.spacing(1),
-    [theme.breakpoints.up("1020")]: {
-      // marginLeft: theme.spacing(22),
-    },
   },
   card: {
     alignItems: 'center',
     alignContent: 'center',
-    verticalAlign: "middle",
+    verticalAlign: 'middle',
     backgroundImage: 'url(./_MG_0509.jpg)',
     backgroundRepeat: 'no-repeat',
     WebkitBackgroundSize: 'cover',
-    // boxShadow: "0 5px 8px 0 rgba(0, 0, 0, 0.3)",
-    [theme.breakpoints.down("768")]: {
+    [theme.breakpoints.down('768')]: {
       marginTop: 75,
     },
-    [theme.breakpoints.up("768")]: {
+    [theme.breakpoints.up('768')]: {
       marginTop: 75,
       height: '150%',
     },
-    [theme.breakpoints.up("1020")]: {
+    [theme.breakpoints.up('1020')]: {
       marginTop: 150,
       marginLeft: 200,
       height: '150%',
     },
-    [theme.breakpoints.up("1025")]: {
+    [theme.breakpoints.up('1025')]: {
       marginTop: 75,
       marginLeft: 0,
     },
@@ -67,8 +68,8 @@ const useStyles = makeStyles((theme) => ({
     border: 0,
     paddingLeft: 20,
     backgroundColor: 'rgba(255, 255, 255, 0.5)',
-    boxShadow: "0 4px 8px 0 rgba(255, 255, 255, 0.2), 0 6px 20px 0 rgba(255, 255, 255, 0.19);",
-    [theme.breakpoints.up("1020")]: {
+    boxShadow: '0 4px 8px 0 rgba(255, 255, 255, 0.2), 0 6px 20px 0 rgba(255, 255, 255, 0.19);',
+    [theme.breakpoints.up('1020')]: {
       marginLeft: 0,
       marginTop: 20,
     },
@@ -78,22 +79,22 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function Checkout({menuItems}) {
+export default function Checkout({ menuItems }) {
   const { total, cartItems, checkout, handleCheckout } = useContext(CartContext);
 
   const classes = useStyles();
 
   return (
-    <Grid container direction="row">
+    <Grid container direction="row" className={classes.root}>
       <TopNavBar></TopNavBar>
       <Navigation menuItems={menuItems}></Navigation>
-      <Container maxWidth="lg">
+      <Container maxWidth="lg" className={classes.contentWrap}>
         <Card className={classes.card}>
           <CardContent>
             <Card className={classes.second}>
               <Grid item xs={12} className={classes.gridItem}>
-              <Typography className={classes.heading} variant="h2" component="h1">
-                Checkout
+                <Typography className={classes.heading} variant="h2" component="h1">
+                  Checkout
                 </Typography>
               </Grid>
               <Grid item xs={12} className={classes.gridItem}>
@@ -111,6 +112,7 @@ export default function Checkout({menuItems}) {
           </CardContent>
         </Card>
       </Container>
+      <Footer />
     </Grid>
   );
 }
